5165dfd712d1703d7b113daed83982ae1f268c69
muskan.r2421 <muskan.r@cisinlabs.com>
2022-09-20 11:47:56 +0530

    Date Conversion changes

--- CisMobileLibrary/src/main/java/com/example/cismobilelibrary/DateConversion.kt
+++ CisMobileLibrary/src/main/java/com/example/cismobilelibrary/DateConversion.kt
@@ -9,32 +9,30 @@ import java.util.*
 
 class DateConversion {
 
-    companion object{
+    companion object {
 
-        val ddMMYYYY  = "dd MM yyyy"
-        val dd_MM_YYYY  = "dd-MM-yyyy"
+        val ddMMYYYY = "dd MM yyyy"
+        val dd_MM_YYYY = "dd-MM-yyyy"
         val ddMMYYYYSlash = "dd/MM/yyyy"
         val YYYYMMdd = "yyyy MM dd"
         val YYYY_MM_dd = "yyyy-MM-dd"
         val YYYYMMddSlash = "yyyy/MM/dd"
         val ddMMMYYYY = "dd MMM yyyy"
-        val ddMMYYYYhhmmaa = "dd MM YYYY hh:mm aa"
-        val dd_MM_YYYYhhmmaa = "dd-MM-YYYY hh:mm aa"
-        val ddMMYYYYhhmmaaSlash = "dd/MM/YYYY hh:mm aa"
-        val ddMMYYYYHHmm = "dd MM YYYY HH:mm"
-        val dd_MM_YYYYHHmm = "dd-MM-YYYY HH:mm"
-        val ddMMYYYYHHmmsSplash = "dd/MM/YYYY HH:mm"
+        val ddMMYYYYhhmmaa = "dd MM yyyy hh:mm aa"
+        val dd_MM_YYYYhhmmaa = "dd-MM-yyyy hh:mm aa"
+        val ddMMYYYYhhmmaaSlash = "dd/MM/yyyy hh:mm aa"
+        val ddMMYYYYHHmm = "dd MM yyyy HH:mm"
+        val dd_MM_YYYYHHmm = "dd-MM-yyyy HH:mm"
+        val ddMMYYYYHHmmsSplash = "dd/MM/yyyy HH:mm"
         val HHmm = "HH:mm"
         val hhmmaa = "hh:mm aa"
 
-        @SuppressLint("SimpleDateFormat")
         fun getCurrentDateTime(): String? {
             val smsTime = Calendar.getInstance()
             val simpleDateFormat = SimpleDateFormat("dd MMM yyyy'&'HH:mm:ss")
             return simpleDateFormat.format(smsTime.time)
         }
 
-        @SuppressLint("SimpleDateFormat")
         fun convert(value: String, input: String, output: String): String {
             val originalFormat = SimpleDateFormat(input)
             val targetFormat = SimpleDateFormat(output)
